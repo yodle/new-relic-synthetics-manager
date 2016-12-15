19d5fe7a493da81085567b9e9932c9b5ec4a3dba
David Bradford <david.bradford@yodle.com>
2016-12-15 11:49:55 -0600

    Improve ImportMonitorOrchestrator test coverage

--- test/lib/orchestrator/ImportMonitorOrchestratorTest.js
+++ test/lib/orchestrator/ImportMonitorOrchestratorTest.js
@@ -8,19 +8,27 @@ chai.use(tdChai(td));
 const importMonitorOrchestratorFactory = require('../../../lib/orchestrator/ImportMonitorOrchestrator');
 
 describe('ImportMonitorOrchestrator', () => {
+    const expectedSyntheticName = 'syntheticName';
     const expectedSyntheticId = 'syntheticId';
     const expectedFilename = 'syntheticFilename.js';
 
-    const defaultsMock = {};
+    const defaultsMock = {
+        syntheticsContent: ''
+    };
     const syntheticsListFileServiceMock = {
         addSynthetic: td.function()
     };
-    const syntheticsFileServiceMock = {};
+    const syntheticsFileServiceMock = {
+        createFile: td.function()
+    };
     const newRelicServiceMock = {
         getSynthetic: td.function(),
         getMonitorScript: td.function()
     };
-    const syntheticInfoMock = {};
+    const syntheticInfoMock = {
+        name: expectedSyntheticName,
+        id: expectedSyntheticId
+    };
 
     it('should fail if it cannot find info in New Relic', () => {
         const expectedError = 'Could not find synthetic';
@@ -42,8 +50,7 @@ describe('ImportMonitorOrchestrator', () => {
         }).should.throw(expectedError);
     });
 
-    it('should get synthetic from New Relic and write it to disk', () => {
-
+    it ('should get synthetic from New Relic and add it to synthetic list file', () => {
         td.when(newRelicServiceMock.getSynthetic(
             expectedSyntheticId,
             td.callback
@@ -58,6 +65,78 @@ describe('ImportMonitorOrchestrator', () => {
 
         importMonitorOrchestrator.importSynthetic(expectedSyntheticId, expectedFilename);
 
+        syntheticsListFileServiceMock.addSynthetic.should.have.been.calledWith(
+            expectedSyntheticId,
+            expectedSyntheticName,
+            expectedFilename,
+            td.callback
+        );
     });
 
+    it ('should get synthetic code from New Relic and add it to synthetic file', () => {
+        const expectedContent = 'expected\nsynthetic\nfile\ncontent';
+
+        td.when(newRelicServiceMock.getSynthetic(
+            expectedSyntheticId,
+            td.callback
+        )).thenCallback(syntheticInfoMock);
+
+        td.when(newRelicServiceMock.getMonitorScript(
+            expectedSyntheticId,
+            td.callback
+        )).thenCallback(Buffer.from(expectedContent).toString('base64'));
+
+        td.when(syntheticsFileServiceMock.createFile(
+            td.matchers.isA(String),
+            td.matchers.isA(String),
+            td.callback
+        )).thenCallback();
+
+        td.when(syntheticsListFileServiceMock.addSynthetic(
+            td.matchers.isA(String),
+            td.matchers.isA(String),
+            td.matchers.isA(String),
+            td.callback
+        )).thenCallback();
+
+        const importMonitorOrchestrator = importMonitorOrchestratorFactory(
+            syntheticsListFileServiceMock,
+            syntheticsFileServiceMock,
+            newRelicServiceMock,
+            defaultsMock
+        );
+
+        importMonitorOrchestrator.importSynthetic(expectedSyntheticId, expectedFilename);
+
+        syntheticsFileServiceMock.createFile.should.have.been.calledWith(
+            expectedFilename,
+            expectedContent,
+            td.callback
+        );
+    });
+
+    it ('should fail if New Relic cannot get synthetic code', () => {
+        const expectedError = 'Could not read synthetic code';
+
+        td.when(newRelicServiceMock.getSynthetic(
+            expectedSyntheticId,
+            td.callback
+        )).thenCallback(syntheticInfoMock);
+
+        td.when(newRelicServiceMock.getMonitorScript(
+            expectedSyntheticId,
+            td.callback
+        )).thenCallback(null, expectedError);
+
+        const importMonitorOrchestrator = importMonitorOrchestratorFactory(
+            syntheticsListFileServiceMock,
+            syntheticsFileServiceMock,
+            newRelicServiceMock,
+            defaultsMock
+        );
+
+        (() => {
+            importMonitorOrchestrator.importSynthetic(expectedSyntheticId, expectedFilename);
+        }).should.throw(expectedError);
+    });
 });
